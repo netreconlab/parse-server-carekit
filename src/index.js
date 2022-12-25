dc3001246c97e0df2061acc0dea398bd57bffc38
Corey's iMac <coreyearleon@icloud.com>
2022-12-25 14:29:14 -0500

    don't use import

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import CareKitServer from './CareKitServer';
+const { CareKitServer } = require('./CareKitServer');
+//import CareKitServer from './CareKitServer';
 
 export default CareKitServer;
 module.exports = CareKitServer;
