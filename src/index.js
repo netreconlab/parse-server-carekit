f200ee8096382d40c1f745172fcbc1372abf3528
Corey's iMac <coreyearleon@icloud.com>
2022-12-25 15:40:54 -0500

    fix babel

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 const { CareKitServer } = require('./CareKitServer');
-//import CareKitServer from './CareKitServer';
 
 export default CareKitServer;
 export {
   CareKitServer
 };
-// module.exports = CareKitServer;
